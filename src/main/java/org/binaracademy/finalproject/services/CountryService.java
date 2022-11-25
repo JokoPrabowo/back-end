3b808ee7ce299f7bb3f6ee631646eadd491b716c
Farhan Nuryadi <farhannuryadi6@gmail.com>
2022-11-25 23:06:15 +0700

    done seeder data country and city

--- src/main/java/org/binaracademy/finalproject/services/CountryService.java
+++ src/main/java/org/binaracademy/finalproject/services/CountryService.java
@@ -3,7 +3,6 @@ package org.binaracademy.finalproject.services;
 import org.binaracademy.finalproject.entity.CountryEntity;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CountryService {
 
