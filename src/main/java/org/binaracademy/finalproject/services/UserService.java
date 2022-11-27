518c84e6aebd802cc5051dce774815c49f7d8a69
ingwerludwig <ingwerflash@gmail.com>
2022-11-27 19:46:28 +0700

    feat: add user service and impl

--- src/main/java/org/binaracademy/finalproject/services/UserService.java
+++ src/main/java/org/binaracademy/finalproject/services/UserService.java
@@ -1,8 +1,11 @@
 package org.binaracademy.finalproject.services;
 
+import org.binaracademy.finalproject.entity.GuestEntity;
 import org.binaracademy.finalproject.entity.UserEntity;
 
 public interface UserService {
+    public UserEntity create(UserEntity data);
 
-    public UserEntity
+    public void processOAuthPostLogin(String email, String nama);
+
 }
