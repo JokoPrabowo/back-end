948f1389fb6dee18c2da383befef2094e439e28c
ingwerludwig <ingwerflash@gmail.com>
2022-12-16 16:51:28 +0700

    feat: add endpoint edit_profile user

--- src/main/java/org/binaracademy/finalproject/services/UsersDetailsService.java
+++ src/main/java/org/binaracademy/finalproject/services/UsersDetailsService.java
@@ -5,4 +5,5 @@ import org.binaracademy.finalproject.entity.UserDetailsEntity;
 public interface UsersDetailsService {
 
     UserDetailsEntity create (UserDetailsEntity userdetailsEntity);
+    UserDetailsEntity findByUserid (Long user_id);
 }
