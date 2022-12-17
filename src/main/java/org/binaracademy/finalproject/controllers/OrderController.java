69f0427252966dc9bf5289ac08a17dd65e336dbf
Joko Prabowo <63594259+JokoPrabowo@users.noreply.github.com>
2022-12-17 14:44:21 +0700

    Update annotation

--- src/main/java/org/binaracademy/finalproject/controllers/OrderController.java
+++ src/main/java/org/binaracademy/finalproject/controllers/OrderController.java
@@ -22,10 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
@@ -33,6 +30,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
