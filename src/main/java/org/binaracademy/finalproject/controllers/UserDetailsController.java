9fc76354693a5324bbf43a4b190756704ebe8e56
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-12-20 22:38:31 +0700

    add new endpoint

--- src/main/java/org/binaracademy/finalproject/controllers/UserDetailsController.java
+++ src/main/java/org/binaracademy/finalproject/controllers/UserDetailsController.java
@@ -4,14 +4,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.binaracademy.finalproject.dto.Request.UserDetailsRequest;
 import org.binaracademy.finalproject.dto.ResponseData;
 import org.binaracademy.finalproject.entity.UserDetailsEntity;
+import org.binaracademy.finalproject.repositories.UsersDetailsRepo;
 import org.binaracademy.finalproject.security.jwt.JwtDecode;
 import org.binaracademy.finalproject.services.UsersDetailsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @Slf4j
@@ -21,7 +27,51 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "UserDetails", description = "Operation about User Details")
 public class UserDetailsController {
     private final UsersDetailsService userDetailsService;
+    private final UsersDetailsRepo usersDetailsRepo;
     private final JwtDecode jwtDecode;
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/user/edit_profile/update")
+    public ResponseEntity<ResponseData<Object>> update(@Valid @RequestBody UserDetailsRequest data, Errors errors){
+        ResponseData<Object> res = new ResponseData();
+        try{
+            if(errors.hasErrors()){
+                res.setSuccess(false);
+                res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+                res.setMessage("Failed! ");
+                res.setData(null);
+                errors.getAllErrors().forEach(x ->{
+                    System.out.println(x);
+                });
+                return ResponseEntity.badRequest().body(res);
+            }
+            Optional<UserDetailsEntity> sample = usersDetailsRepo.findById(jwtDecode.decode().getUserId());
+            if (sample.isEmpty()){
+                res.setSuccess(false);
+                res.setStatusCode(HttpStatus.NOT_FOUND.value());
+                res.setMessage("User not found!");
+                res.setData(null);
+            }
+            UserDetailsEntity user = userDetailsService.update(
+                    new UserDetailsEntity(
+                            data.getBirthDate(),
+                            data.getGender(),
+                            data.getAddress(),
+                            data.getUser_id()), jwtDecode.decode().getUserId());
+            res.setSuccess(true);
+            res.setStatusCode(HttpStatus.CREATED.value());
+            res.setMessage("Successfully!");
+            res.setData(user);
+
+            return ResponseEntity.ok(res);
+        }catch (Exception e){
+            res.setSuccess(false);
+            res.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            res.setMessage("Failed!");
+            res.setData(null);
+            return ResponseEntity.internalServerError().body(res);
+        }
+    }
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/get/user/edit_profile")
