55662e25f816bd494bec6467979c7925c7b3f7a8
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-12-20 20:20:03 +0700

    update attribute

--- src/main/java/org/binaracademy/finalproject/dto/Response/AirportResponse.java
+++ src/main/java/org/binaracademy/finalproject/dto/Response/AirportResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +15,5 @@ public class AirportResponse {
     private Long id;
     private String airportName;
     private String cityName;
+    private List<PesawatResponse> pesawat;
 }
