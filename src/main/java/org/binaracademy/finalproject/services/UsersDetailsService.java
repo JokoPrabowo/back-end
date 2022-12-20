955a1e38c279662c3d3eebdd74a94b6768d963e2
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-12-20 22:38:14 +0700

    add new service

--- src/main/java/org/binaracademy/finalproject/services/UsersDetailsService.java
+++ src/main/java/org/binaracademy/finalproject/services/UsersDetailsService.java
@@ -5,5 +5,6 @@ import org.binaracademy.finalproject.entity.UserDetailsEntity;
 public interface UsersDetailsService {
 
     UserDetailsEntity create (UserDetailsEntity userdetailsEntity);
+    UserDetailsEntity update (UserDetailsEntity userdetailsEntity, Long id);
     UserDetailsEntity findByUserid (Long user_id);
 }
--- src/main/java/org/binaracademy/finalproject/services/impl/UsersDetailsServiceImpl.java
+++ src/main/java/org/binaracademy/finalproject/services/impl/UsersDetailsServiceImpl.java
@@ -9,6 +9,9 @@ import org.binaracademy.finalproject.services.UsersDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @Slf4j
 public class UsersDetailsServiceImpl implements UsersDetailsService {
@@ -24,6 +27,21 @@ public class UsersDetailsServiceImpl implements UsersDetailsService {
             UserDetailsEntity userDetails = userDetailRepo.save(userdetailsEntity);
             log.info("User Details has been created");
             return userDetails;
+        }catch (Exception e){
+            log.error(ERROR_FOUND, e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public UserDetailsEntity update(UserDetailsEntity userdetailsEntity, Long id) {
+        try{
+            UserDetailsEntity data = userDetailRepo.findById(id).get();
+            data.setAddress(userdetailsEntity.getAddress());
+            data.setBirthDate(userdetailsEntity.getBirthDate());
+            data.setGender(userdetailsEntity.getGender());
+            data.setUpdateAt(LocalDateTime.now());
+            return userDetailRepo.save(data);
         }catch (Exception e){
             log.error(ERROR_FOUND, e.getMessage());
             return null;
