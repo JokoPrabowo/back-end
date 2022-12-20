d0cad6e7cb0be6b6d69d001e0d2d95ccb8b31d5a
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-12-20 18:27:01 +0700

    add new method to get item

--- src/main/java/org/binaracademy/finalproject/services/PesawatService.java
+++ src/main/java/org/binaracademy/finalproject/services/PesawatService.java
@@ -3,6 +3,11 @@ package org.binaracademy.finalproject.services;
 import org.binaracademy.finalproject.data.PesawatData;
 import org.binaracademy.finalproject.entity.PesawatEntity;
 
+import java.util.List;
+
 public interface PesawatService {
     public PesawatEntity create(PesawatEntity data);
+    public List<PesawatEntity> getAll();
+    public List<PesawatEntity> getByAirportId(Long id);
+    public PesawatEntity getById(Long id);
 }
