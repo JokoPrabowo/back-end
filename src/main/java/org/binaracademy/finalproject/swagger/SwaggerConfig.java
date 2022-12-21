abc6b1a067f639208e7a96a9a749f30df5e6cc81
Joko Prabowo <jokoprabowo4550@gmail.com>
2022-12-21 17:17:29 +0700

    fixing

--- src/main/java/org/binaracademy/finalproject/swagger/SwaggerConfig.java
+++ src/main/java/org/binaracademy/finalproject/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.binaracademy.finalproject.dto.swagger;
+package org.binaracademy.finalproject.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
